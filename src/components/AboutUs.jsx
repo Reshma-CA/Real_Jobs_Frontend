fcaf1bcc9db9a6f8295ca838c392179dfebd0e89
Reshma.CA <reshmacaci@gmail.com>
2024-06-23 13:01:25 +0530

    eleventh

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -30,21 +30,28 @@ const AboutUs = () => {
           justifyContent: 'center',
         }}
       >
-        <Box
-          sx={{
-            width: { xs: '90%', sm: '70%', md: '50%', lg: '40%' },
-            padding: 2,
-            backgroundColor: 'rgba(255, 255, 255, 0.8)',
-            textAlign: 'center',
-          }}
-        >
-          <Typography variant='h6' sx={{ fontWeight: 'bold', color:'#cf1b6c' }} pt={1}>
-          Realjobs Tech Solutions is a leading job provideing company specializing in cutting-edge 
-          solutions for the healthcare industry. With a focus on innovation and quality, 
-          we strive to empower healthcare providers with advanced technology to improve job care and streamline operations.
-          </Typography>
-          
-        </Box>
+            <Box
+        sx={{
+          width: { xs: '90%', sm: '70%', md: '50%', lg: '40%' },
+          padding: 2,
+          backgroundColor: 'rgba(255, 255, 255, 0.8)',
+          textAlign: 'center',
+          margin: 'auto', // Center the Box on the page
+          borderRadius: '8px', // Add rounded corners for a polished look
+          boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.1)' // Add a subtle shadow for depth
+        }}
+      >
+        <Typography variant='h5' sx={{ fontWeight: 'bold', color:'#cf1b6c' }} pt={1}>
+          Welcome to Realjobs
+        </Typography>
+        <Typography variant='body1' sx={{ color: '#333', mt: 2, fontWeight: 'bold' }}>
+          This platform for skilled professionals for all your home service needs. Whether you need a plumber, electrician, painter, coconut plucker, or cleaner, we've got you covered.
+        </Typography>
+        <Typography variant='body1' sx={{ color: '#333', mt: 2 ,fontWeight: 'bold'}}>
+          Our mission is to connect you with reliable and experienced tradespeople who can efficiently handle any job you need done around your house.
+        </Typography>
+      </Box>
+
       </Box>
       <Footer/>
     </Box>
